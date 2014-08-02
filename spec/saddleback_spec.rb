802ef971a8e6d889442d58cf1b1c992caacfab88
Al Wold <alwold@alwold.com>
2014-08-01 21:42:54 -0700

    update classes with recent data

--- spec/saddleback_spec.rb
+++ spec/saddleback_spec.rb
@@ -3,18 +3,18 @@ require 'class_info'
 
 describe SaddlebackScheduleScraper, '#get_class_info' do
   scraper = SaddlebackScheduleScraper.new
-  it "open class shows open status" do
-    scraper.get_class_status("20132", "12195").should eq(:open)
+  it 'open class shows open status' do
+    scraper.get_class_status('20143', '21500').should eq(:open)
   end
-  it "closed class shows closed status" do
-    scraper.get_class_status("20132", "12305").should eq(:closed)
+  it 'closed class shows closed status' do
+    scraper.get_class_status('20143', '12830').should eq(:closed)
   end
-  it "class info can be loaded" do
-    info = scraper.get_class_info('20132', '12305')
-    info.name.should eq("ENG 300 - BEGINNING WRITING")
-    info.schedule.should eq("M W 8:00AM - 10:50")
+  it 'class info can be loaded' do
+    info = scraper.get_class_info('20143', '12830')
+    info.name.should eq('ACCT 1A - FINANCIAL ACCOUNTING')
+    info.schedule.should eq('M W 7:00PM - 9:15')
   end
-  it "returns nil for non-existent class" do
+  it 'returns nil for non-existent class' do
     scraper.get_class_status('20132', '12398').should eq(nil)
     scraper.get_class_info('20132', '12398').should eq(nil)
   end
